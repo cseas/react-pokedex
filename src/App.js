147af0380760ce6ba599e05dc3b3355d827dca91
cseas <contact.abhijeetsingh@gmail.com>
2020-05-31 13:41:25 +0530

    Adding HTML content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const PokeAPI = "https://pokeapi.co/api/v2/pokemon?limit=15";
 
 function App() {
+  let pokemons = [];
+  async function getPokemons() {
+    await fetch(PokeAPI).then(function (response) {
+      response.json().then(function (data) {
+        console.log(data.results);
+      });
+    });
+  }
+
+  getPokemons();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // main app
+    <div className="app">
+      {/* title-bar */}
+      <div className="title-bar">
+        <h3>PokeDex</h3>
+      </div>
+
+      <div className="container">
+        {/* pokemon card */}
+        <div className="card">
+          <img
+            src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+            width={100}
+          />
+          <h4>
+            <b>Bulbasaur</b>
+          </h4>
+          <p>Id: 1</p>
+          <p>Type: 1</p>
+        </div>
+        {/* pokemon card end */}
+      </div>
     </div>
+    // main app end
   );
 }
 

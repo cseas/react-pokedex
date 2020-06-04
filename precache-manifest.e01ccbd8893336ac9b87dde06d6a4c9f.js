a535936e28ab20c90fc5ec857ee89e464d84a617
cseas <contact.abhijeetsingh@gmail.com>
2020-06-05 00:43:58 +0530

    Updates

---
+++ precache-manifest.e01ccbd8893336ac9b87dde06d6a4c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10b4f83f497becb13293befb5822dfb",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "e2eeac2d35ce81d3e204",
+    "url": "/react-pokedex/static/css/main.67e84045.chunk.css"
+  },
+  {
+    "revision": "10e37cd498e3f1aaaf23",
+    "url": "/react-pokedex/static/js/2.9a3eec03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokedex/static/js/2.9a3eec03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2eeac2d35ce81d3e204",
+    "url": "/react-pokedex/static/js/main.deb6bb20.chunk.js"
+  },
+  {
+    "revision": "68d4e79d7264ca9d2b51",
+    "url": "/react-pokedex/static/js/runtime-main.1c0e1c23.js"
+  }
+]);
